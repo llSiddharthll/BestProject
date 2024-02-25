cc54ddc4438079bd230a20e196a6e2f509bcca0e
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-25 14:42:31 +0530

    ok

--- api/models.py
+++ api/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class ChatModel(models.Model):
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
